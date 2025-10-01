330b5c2f10b26e455445dc0e11e837a1951de36b
Pilape <133979545+Pilape@users.noreply.github.com>
2025-10-01 14:34:15 +0200

    Added localstorage and style
    Used localstorage to prevent duplicate votes.
    
    Also styled up the frontend

--- votepage/script.js
+++ votepage/script.js
@@ -1,11 +1,10 @@
 const url = "https://pollapi.it3.iktim.no";
-var prev_poll_name = null
-var current_poll_name = null
+var current_poll_name = null;
 
 // Update the poll on reload
 window.onload = updatePoll();
 // Update the poll every 2 seconds
-setInterval(async function() {updatePoll()}, 2000)
+setInterval(async function() {updatePoll()}, 2000);
 
 async function updatePoll() {
     await fetch(url + "/poll").then(
@@ -14,9 +13,7 @@ async function updatePoll() {
 
         // Verify poll names
         if (data.name != current_poll_name) { // New poll
-            prev_poll_name = current_poll_name;
             current_poll_name = data.name;
-            localStorage.setItem("has_voted", true);
         }
 
         createPollHtml(data);
@@ -28,7 +25,9 @@ async function updatePoll() {
 }
 
 async function castVote(name) {
-    if (!canVote) return;
+    if (!canVote()) {
+        return;
+    } 
 
     await fetch(url + "/vote", {
         method: "POST",
@@ -39,14 +38,13 @@ async function castVote(name) {
             "Content-type": "application/json"
         }
     }).then(data => {
-        localStorage.setItem("has_voted", false);
+        localStorage.setItem("prev_poll_name", current_poll_name);
     });
 }
 
 function canVote() {
-    if (localStorage.getItem("has_voted") == false || localStorage.getItem("has_voted") == null) return true;
-    if (localStorage.getItem("prev_poll_name") != prev_poll_name || localStorage.getItem("prev_poll_name") == null) return true;
-    return false;
+    if (localStorage.getItem("prev_poll_name") == current_poll_name) return false;
+    return true;
 }
 
 function createPollHtml(poll_data) {
